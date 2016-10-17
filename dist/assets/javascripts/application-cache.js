c8ef690cb85c4c104959e27e34f6eb1d3469108a
Eduardo Matos <eduardoj.matos@gmail.com>
2016-10-16 23:54:16 -0200

    Lesson 1 finished (update application cache manually)

--- dist/assets/javascripts/application-cache.js
+++ dist/assets/javascripts/application-cache.js
@@ -1,30 +1,56 @@
 (function () {
-  applicationCache.onobsolete = function(event) {
-    alert("applicationCache obsolete!");
-    console.log("obsolted cache", event.type);
+  const appCache = applicationCache;
+
+  appCache.onobsolete = function(event) {
+    console.log('obsoleted cache', event.type);
   };
 
-  applicationCache.onnoupdate = function (event) {
-    console.info("applicationCache updated!");
+  appCache.onnoupdate = function (event) {
+    console.info('appCache updated!');
   };
 
-  applicationCache.onupdateready = function (event) {
-    console.info("applicationCache update ready!");
+  appCache.onupdateready = function (event) {
+    console.info('appCache update ready!');
   };
 
-  applicationCache.onchecking = function (event) {
-    console.log("applicationCache checking...", event.type);
+  appCache.onchecking = function (event) {
+    console.log('appCache checking...', event.type);
   };
 
-  applicationCache.ondownloading = function (event) {
-    console.log("downloading...", event.type);
+  appCache.ondownloading = function (event) {
+    console.log('downloading...', event.type);
   };
 
-  applicationCache.onprogress = function (event) {
-    console.info("loaded:", (event.loaded / event.total) * 100, "%");
+  appCache.onprogress = function (event) {
+    console.info('loaded:', (event.loaded / event.total) * 100, '%');
   };
 
-  applicationCache.oncached = function (event) {
-    console.log("cached!", event.type);
+  appCache.oncached = function (event) {
+    console.log('cached!', event.type);
   };
+
+  const checkStatus = (status) => {
+    if (status === appCache.UPDATEREADY) {
+      appCache.swapCache();
+    }
+
+    if (status === appCache.IDLE) {
+      window.location.reload();
+      return;
+    }
+
+    setTimeout(() => {
+      checkStatus(appCache.status);
+    }, 250);
+  };
+
+  /*
+   * Updating cache event
+   */
+  const updateCacheButton = document.querySelector('#update-cache');
+
+  updateCacheButton.addEventListener('click', (event) => {
+    appCache.update();
+    checkStatus(appCache.status);
+  });
 })();
